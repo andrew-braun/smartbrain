e9efcf7b1367c30b0873f4e63acc6290ce161c50
Andrew Braun <andrew@andrewbraun.dev>
2020-12-05 11:13:28 +0400

    feat: Add type=password to signIn and register page password fields

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -3,119 +3,127 @@ import styles from "./register.module.css";
 import "./register.module.css";
 
 class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      registerName: "",
-      nameValid: "",
-      registerEmail: "",
-      emailValid: "",
-      registerPassword: "",
-      passwordValid: ""
-    }
-  }
-  onNameChange = (event) => {
-    this.setState({registerName: event.target.value})
-  }
-  onEmailChange = (event) => {
-    this.setState({registerEmail: event.target.value})
-  }
-  onPasswordChange = (event) => {
-    if (event.target.value.length >= 3) {
-      this.setState({
-        passwordValid: true,
-        registerPassword: event.target.value,
-      })
-    } else {
-      this.setState({passwordValid: false})
-    }
-  }
-  onRegisterSubmit = () => {
-    fetch("https://cryptic-journey-42100.herokuapp.com/register", {
-      method: "POST",
-      headers: {"CONTENT-TYPE": "application/json"},
-      body: JSON.stringify({
-        name: this.state.registerName,
-        email: this.state.registerEmail,
-        password: this.state.registerPassword
-      })
-    }).then(response => response.json())
-      .then(user => {
-        if (user.id) { 
-          this.props.loadUser(user)
-          this.props.onRouteChange("home")
-        }
-      })
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			registerName: "",
+			nameValid: "",
+			registerEmail: "",
+			emailValid: "",
+			registerPassword: "",
+			passwordValid: "",
+		};
+	}
+	onNameChange = (event) => {
+		this.setState({ registerName: event.target.value });
+	};
+	onEmailChange = (event) => {
+		this.setState({ registerEmail: event.target.value });
+	};
+	onPasswordChange = (event) => {
+		if (event.target.value.length >= 3) {
+			this.setState({
+				passwordValid: true,
+				registerPassword: event.target.value,
+			});
+		} else {
+			this.setState({ passwordValid: false });
+		}
+	};
+	onRegisterSubmit = () => {
+		fetch("https://cryptic-journey-42100.herokuapp.com/register", {
+			method: "POST",
+			headers: { "CONTENT-TYPE": "application/json" },
+			body: JSON.stringify({
+				name: this.state.registerName,
+				email: this.state.registerEmail,
+				password: this.state.registerPassword,
+			}),
+		})
+			.then((response) => response.json())
+			.then((user) => {
+				if (user.id) {
+					this.props.loadUser(user);
+					this.props.onRouteChange("home");
+				}
+			});
+	};
 
-  render() {
-    const { onNameChange, onEmailChange, onPasswordChange, onRegisterSubmit } = this
+	render() {
+		const {
+			onNameChange,
+			onEmailChange,
+			onPasswordChange,
+			onRegisterSubmit,
+		} = this;
 
-    return (
-      <main className={styles.registerMain}>
-        <div className={styles.registrationForm}>
-          <legend className={styles.registerLegend}>Register</legend>
-          <div className={styles.registerElementContainer}>
-            <label
-              className={styles.registerLabel}
-              id={styles.registerNameLabel}
-              htmlFor={styles.registerName}
-            >
-              Name
-            </label>
-            <input
-              className={styles.registerInput}
-              id={styles.registerName}
-              type="text"
-              placeholder="Enter your name"
-              onChange={onNameChange}
-            />
-          </div>
-          <div className={styles.registerElementContainer}>
-            <label
-              className={styles.registerLabel}
-              id={styles.registerEmailLabel}
-              htmlFor={styles.emailAddress}
-            >
-              Email
-            </label>
-            <input
-              className={styles.registerInput}
-              id={styles.emailAddress}
-              type="email"
-              placeholder="Enter your email address"
-              onChange={onEmailChange}
-            />
-          </div>
-          <div className={styles.registerElementContainer}>
-            <label
-              className={styles.registerLabel}
-              id={styles.registerPasswordLabel}
-              htmlFor={styles.password}
-            >
-              Password
-            </label>
-            <input 
-              className={styles.registerInput} 
-              id={styles.password} 
-              placeholder="Enter your password"
-              onChange={onPasswordChange}
-            />
-          </div>
-          
-          <div className={styles.registerElementContainer}>
-              <input 
-                  className={styles.registerButton} 
-                  id={styles.registerButton}
-                  type="submit"
-                  value="Register"
-                  onClick={onRegisterSubmit}
-                  />
-          </div>
-        </div>
-      </main>
-    );
-  }
-};
+		return (
+			<main className={styles.registerMain}>
+				<div className={styles.registrationForm}>
+					<legend className={styles.registerLegend}>Register</legend>
+					<div className={styles.registerElementContainer}>
+						<label
+							className={styles.registerLabel}
+							id={styles.registerNameLabel}
+							htmlFor="registerName"
+						>
+							Name
+						</label>
+						<input
+							className={styles.registerInput}
+							id="registerName"
+							type="text"
+							placeholder="Enter your name"
+							onChange={onNameChange}
+						/>
+					</div>
+					<div className={styles.registerElementContainer}>
+						<label
+							className={styles.registerLabel}
+							id={styles.registerEmailLabel}
+							htmlFor={styles.emailAddress}
+						>
+							Email
+						</label>
+						<input
+							className={styles.registerInput}
+							id={styles.emailAddress}
+							type="email"
+							placeholder="Enter your email address"
+							onChange={onEmailChange}
+						/>
+					</div>
+					<div className={styles.registerElementContainer}>
+						<label
+							className={styles.registerLabel}
+							id={styles.registerPasswordLabel}
+							htmlFor="password"
+						>
+							Password
+						</label>
+						<input
+							className={styles.registerInput}
+							id="password"
+							name="password"
+							type="password"
+							placeholder="Enter your password"
+							onChange={onPasswordChange}
+						/>
+					</div>
+
+					<div className={styles.registerElementContainer}>
+						<input
+							className={styles.registerButton}
+							id={styles.registerButton}
+							type="submit"
+							value="Register"
+							onClick={onRegisterSubmit}
+						/>
+					</div>
+				</div>
+			</main>
+		);
+	}
+}
 
 export default Register;
--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -3,99 +3,101 @@ import styles from "./signin.module.css";
 import "./signin.module.css";
 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      signInEmail: "",
-      signInPassword: ""
-    }
-  }
-  onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
-  }
-  onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
-  }
-  onSubmitSignIn = () => {
-    fetch("https://cryptic-journey-42100.herokuapp.com/signin", {
-      method: "post",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        email: this.state.signInEmail,
-        password: this.state.signInPassword
-      })
-    }).then(response => response.json())
-      .then(user => {
-        
-        if (user.id) {
-          this.props.onRouteChange("home");
-          this.props.loadUser(user);
-        }
-      })
-  }
-  render() {
-    const { onRouteChange } = this.props;
-    const { onEmailChange, onPasswordChange, onSubmitSignIn } =  this;
-    return (
-      <main className={styles.signInMain}>
-        <div className={styles.signInForm}>
-          <legend className={styles.signInLegend}>Sign In</legend>
-          <div className={styles.signInElementContainer}>
-            <label
-              className={styles.signInLabel}
-              id={styles.signInEmailLabel}
-              htmlFor={styles.emailAddress}
-            >
-              Email
-            </label>
-            <input
-              className={styles.signInInput}
-              id={styles.emailAddress}
-              type="email"
-              placeholder="Enter your email address"
-              onChange={onEmailChange}
-            />
-          </div>
-          <div className={styles.signInElementContainer}>
-            <label
-              className={styles.signInLabel}
-              id={styles.signInPasswordLabel}
-              htmlFor={styles.password}
-            >
-              Password
-            </label>
-            <input 
-              className={styles.signInInput} 
-              id={styles.password} 
-              placeholder="Enter your password"
-              onChange = {onPasswordChange}
-            />
-          </div>
-          
-          <div className={styles.signInElementContainer}>
-              <input
-              type="button"
-              value="Register"
-              className={styles.registerButton} 
-              id={styles.registerButton}
-              tabIndex="-2"
-              onClick={() => onRouteChange("register")}
-              />
+	constructor(props) {
+		super(props);
+		this.state = {
+			signInEmail: "",
+			signInPassword: "",
+		};
+	}
+	onEmailChange = (event) => {
+		this.setState({ signInEmail: event.target.value });
+	};
+	onPasswordChange = (event) => {
+		this.setState({ signInPassword: event.target.value });
+	};
+	onSubmitSignIn = () => {
+		fetch("https://cryptic-journey-42100.herokuapp.com/signin", {
+			method: "post",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: this.state.signInEmail,
+				password: this.state.signInPassword,
+			}),
+		})
+			.then((response) => response.json())
+			.then((user) => {
+				if (user.id) {
+					this.props.onRouteChange("home");
+					this.props.loadUser(user);
+				}
+			});
+	};
+	render() {
+		const { onRouteChange } = this.props;
+		const { onEmailChange, onPasswordChange, onSubmitSignIn } = this;
+		return (
+			<main className={styles.signInMain}>
+				<div className={styles.signInForm}>
+					<legend className={styles.signInLegend}>Sign In</legend>
+					<div className={styles.signInElementContainer}>
+						<label
+							className={styles.signInLabel}
+							id={styles.signInEmailLabel}
+							htmlFor="email-address"
+						>
+							Email
+						</label>
+						<input
+							className={styles.signInInput}
+							id="email-address"
+							name="email-address"
+							type="email"
+							placeholder="Enter your email address"
+							onChange={onEmailChange}
+						/>
+					</div>
+					<div className={styles.signInElementContainer}>
+						<label
+							className={styles.signInLabel}
+							id={styles.signInPasswordLabel}
+							htmlFor="password"
+						>
+							Password
+						</label>
+						<input
+							className={styles.signInInput}
+							id="password"
+							type="password"
+							name="password"
+							placeholder="Enter your password"
+							onChange={onPasswordChange}
+						/>
+					</div>
 
-              <input 
-                  className={styles.signInButton} 
-                  id={styles.signInButton}
-                  type="submit"
-                  value="Sign in"
-                  tabIndex="-1"
-                  onClick={onSubmitSignIn}
-                  />
+					<div className={styles.signInElementContainer}>
+						<input
+							type="button"
+							value="Register"
+							className={styles.registerButton}
+							id={styles.registerButton}
+							tabIndex="-2"
+							onClick={() => onRouteChange("register")}
+						/>
 
-          </div>
-        </div>
-      </main>
-    );
-  }
-};
+						<input
+							className={styles.signInButton}
+							id={styles.signInButton}
+							type="submit"
+							value="Sign in"
+							tabIndex="-1"
+							onClick={onSubmitSignIn}
+						/>
+					</div>
+				</div>
+			</main>
+		);
+	}
+}
 
 export default SignIn;
